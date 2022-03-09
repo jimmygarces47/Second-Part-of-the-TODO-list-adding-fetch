32da5c30593ae0bb6410b2ec19960b2519f11509
jimmygarces47 <jimmygarces4@gmail.com>
2022-03-09 16:18:52 +0000

    completo el todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,70 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [Tareas, setTareas] = useState("");
+	const [MostrarTareas, setMostrarTareas] = useState([]);
+	const [buttonOver, setButtonOver] = useState(null);
+	const mappingTareas = MostrarTareas.map((item, i) => {
+		return (
+			<div className="container" key={i}>
+				<li
+					className="list-group-item  "
+					onMouseOver={() => setButtonOver(i)}
+					onMouseLeave={() => setButtonOver(null)}>
+					{item}
+					{buttonOver == i ? (
+						<button
+							className="btn btn-warning"
+							onClick={() => eliminatarea(item)}>
+							X
+						</button>
+					) : (
+						""
+					)}
+				</li>
+			</div>
+		);
+	});
+
+	const eliminatarea = (tarea) => {
+		return setMostrarTareas(MostrarTareas.filter((item) => tarea !== item));
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+		<div className="container  ">
+			<h1 className="">Tareas</h1>
+			<input
+				className=""
+				type="text"
+				placeholder="Que necesitas hacer?"
+				onClick={() => console.log("hice click")}
+				onChange={(e) => setTareas(e.target.value)}
+				value={Tareas}
+			/>
+			<button
+				className="btn btn-success  "
+				onClick={
+					(e) => {
+						Tareas === ""
+							? alert("debe ingresar una tarea")
+							: setMostrarTareas([...MostrarTareas, Tareas]);
+					}
+					//  (
+					// 	Tareas === Tareas
+					// 		? alert("debe borrar la tarea duplicada")
+					// 		: ""
+					// )
+				}>
+				Mostrar Tareas
+			</button>
 			<p>
-				<img src={rigoImage} />
+				{MostrarTareas.length == 0
+					? "No hay tareas, añada una tarea"
+					: mappingTareas}
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<p>{MostrarTareas.length} items left</p>
 		</div>
 	);
 };
